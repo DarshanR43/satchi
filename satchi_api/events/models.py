63f00049219fe91eefa40cb23d6eb37df6df24d3
Shantharam P <shanthu2005best@gmail.com>
2025-08-07 23:40:49 +0530

    minor changes in Events DB table

--- satchi_api/events/models.py
+++ satchi_api/events/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+import datetime
 
 class MainEvent(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    event_id = models.CharField(max_length=100, unique=True, blank=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self.event_id:
+            now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+            self.event_id = f"EVT{now}"
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -12,10 +20,19 @@ class SubEvent(models.Model):
     parent_event = models.ForeignKey(MainEvent, related_name='subevents', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    event_id = models.CharField(max_length=100, unique=True, blank=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self.event_id:
+            now = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+            self.event_id = f"EVT_S{now}"
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return f"{self.name} ({self.parent_event.name})"
-    
+
+
 class SubSubEvent(models.Model):
     parent_event = models.ForeignKey(MainEvent, related_name='subsubevents', on_delete=models.CASCADE)
     parent_subevent = models.ForeignKey(SubEvent, related_name='subsubevents', on_delete=models.CASCADE)
@@ -28,6 +45,12 @@ class SubSubEvent(models.Model):
     minFemaleParticipants = models.PositiveIntegerField(default=0)
     isFacultyMentorRequired = models.BooleanField(default=False)
     
+    event_id = models.CharField(max_length=100, unique=True, blank=True, editable=False)
+    def save(self, *args, **kwargs):
+        if not self.event_id:
+            now = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+            self.event_id = f"EVT_SS{now}"
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name} ({self.parent_subevent.name})"
