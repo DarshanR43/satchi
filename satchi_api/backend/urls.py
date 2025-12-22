eab709147d047322109f63918a81615a465f25f5
aswath-siddharth <144677819+aswath-siddharth@users.noreply.github.com>
2025-12-22 23:37:21 +0530

    Updated files

--- satchi_api/backend/urls.py
+++ satchi_api/backend/urls.py
@@ -19,10 +19,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include([
-        path('', include('api.urls')),
-        path('user/', include('users.urls')),
-        path('events/', include('events.urls')),
-        path('eval/', include('eval.urls')),
-    ])),
+    path('api/', include('api.urls')),
+    path('user/', include('users.urls')),
+    path('events/', include('events.urls')),
+    path('eval/', include('eval.urls')),
 ]
