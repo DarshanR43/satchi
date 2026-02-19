65f96dc6d46bc1e48302cbd7935dc352be167616
aswath-siddharth <144677819+aswath-siddharth@users.noreply.github.com>
2026-02-20 00:19:55 +0530

    qdq

--- satchi-main/src/Pages/Events.jsx
+++ satchi-main/src/Pages/Events.jsx
@@ -5,6 +5,10 @@ import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
 import { Loader } from 'lucide-react';
 
+<<<<<<< Updated upstream
+=======
+import SubSubEventModal from '../Components/events/SubSubEventModal';
+>>>>>>> Stashed changes
 
 
 import EventSidebar from '../Components/events/EventCards';
